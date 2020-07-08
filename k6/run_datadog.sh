7f47615854da7e8551c1e1c88fd360b4b836bd26
Nguyen Hong Minh <47794920+hongminhcbg@users.noreply.github.com>
2020-07-08 09:01:16 +0700

    Update run_datadog.sh

--- k6/run_datadog.sh
+++ k6/run_datadog.sh
@@ -6,7 +6,7 @@ docker run -d \
     -v /proc/:/host/proc/:ro \
     -v /sys/fs/cgroup/:/host/sys/fs/cgroup:ro \
     -e DD_SITE="datadoghq.com" \
-    -e DD_API_KEY=996960a8d30f0f1b8db78d7d88a54efd \
+    -e DD_API_KEY={YOUR_DATADOG_KEY} \
     -e DD_DOGSTATSD_NON_LOCAL_TRAFFIC=1 \
     -p 8125:8125/udp \
     datadog/agent:latest
